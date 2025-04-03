18d771f9eb45624da9784adf9136cab8b19883be
jose manuel machin lorenzo <josemanuelmachinlorenzo@MacBook-Pro-de-jose.local>
2025-04-03 14:49:54 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    TasksModule,
+  ],
+})
+export class AppModule {}
